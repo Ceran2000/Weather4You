60afc56a2eeace7901a63f296e7ea5e2a7a31c3a
Mateusz Ceranka <ceranka@gmail.com>
2025-02-03 22:45:54 +0100

    refactor: app scaffold

--- app/src/main/java/pl/ceranka/weather4you/ui/MainActivity.kt
+++ app/src/main/java/pl/ceranka/weather4you/ui/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
@@ -32,10 +30,8 @@ class MainActivity : ComponentActivity() {
 private fun App() {
     val navController = rememberNavController()
 
-    Scaffold { innerPadding ->
-        AppNavigation(
-            navController = navController,
-            modifier = Modifier.fillMaxSize().padding(innerPadding)
-        )
-    }
+    AppNavigation(
+        navController = navController,
+        modifier = Modifier.fillMaxSize()
+    )
 }
